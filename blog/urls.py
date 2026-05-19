511af14c149ca37c91dc83899db06f350106ec27
@vla78dan <vla78dan@gmail.com>
2026-05-20 02:07:07 +0300

    first_perm_2.4_url_part1_8-14_2

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from blog import views
 
 urlpatterns = [
     path('', views.index),
-    path('about/', views.about),
-    path('contact/', views.contact),
+    path('about/', views.about, kwargs={'name': 'Tom', 'age': 38}),
+    re_path(r'^contact/', views.contact),
+    re_path(r'^user/(?P<name>\D+)/(?P<age>\d+)/', views.user),
+    re_path(r'^user/(?P<name>\D+)/', views.user),
+    re_path(r'^user/', views.user),
 
 ]
