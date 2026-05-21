f7c1c9dd3dd0a7fc695401344f1208b9cfc6ff7d
@vla78dan <vla78dan@gmail.com>
2026-05-21 10:14:34 +0300

    first_perm_2.5-2

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,16 @@
-from django.urls import path, re_path
+from django.urls import path, include
 from blog import views
+
+
+product_patterns = [
+    path('', views.products),
+    path('comments/', views.comments),
+    path('questions/', views.questions),
+]
+
 
 urlpatterns = [
     path('', views.index),
-    path('about/', views.about, kwargs={'name': 'Tom', 'age': 38}),
-    re_path(r'^contact/', views.contact),
-    re_path(r'^user/(?P<name>\D+)/(?P<age>\d+)/', views.user),
-    re_path(r'^user/(?P<name>\D+)/', views.user),
-    re_path(r'^user/', views.user),
+    path('products/<int:id>/', include(product_patterns)),
 
 ]
